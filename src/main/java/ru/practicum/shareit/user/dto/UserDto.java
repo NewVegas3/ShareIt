01c321ed369c98d65d174a52ca6e9e605574169c
Yaroslav <olddwarf3@gmail.com>
2023-10-24 00:52:01 +0600

    тест 1.1

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,14 +2,12 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.validation.Exist;
 import ru.practicum.shareit.validation.ExistUpdateEmail;
 import ru.practicum.shareit.validation.ExistUpdateName;
 import ru.practicum.shareit.validation.New;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
